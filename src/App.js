938be762c2afe0e8d5d94119cf4e606dd7c6c683
Melaniaift <melaniaiftinchi@gmail.com>
2024-10-07 09:55:48 +0300

    theme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { useState } from "react"
+import { useEffect } from 'react';
+
 import './App.css';
 import { Header } from './components/Header';
 import { AddTask } from './components/AddTask';
 import { ShowTask } from './components/ShowTask';
 
 function App() {
-  const [tasklist, setTasklist] = useState([])
+  const [tasklist, setTasklist] = useState(JSON.parse(localStorage.getItem("tasklist")) || []);
   const [task, setTask] = useState({})
+
+  useEffect(() => {
+    localStorage.setItem("tasklist", JSON.stringify(tasklist))
+  }, [tasklist])
 
   return (
     <div className="App">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,14 @@
+import { useState, useEffect } from 'react';
 import Logo from '../assets/sun.png'
 export const Header = () => {
+    const [theme, setTheme] = useState(JSON.parse(localStorage.getItem("theme")) || "dark");
+
+    useEffect(() => {
+        document.documentElement.removeAttribute("class");
+        document.documentElement.classList.add(theme);
+        localStorage.setItem("theme", JSON.stringify(theme))
+    }, [theme])
+
     return (
         <header>
             <div className='logo'>
@@ -7,12 +16,12 @@ export const Header = () => {
                 <spna>My tasks</spna>
             </div>
             <div className='themeSelector'>
-                <span className='light'></span>
-                <span className='medium'></span>
-                <span className='dark'></span>
-                <span className='g1'></span>
-                <span className='g2'></span>
-                <span className='g3'></span>
+                <span onClick={() => setTheme("light")} className={theme === "light" ? "light activeTheme" : "light"}></span>
+                <span onClick={() => setTheme("medium")} className={theme === "medium" ? "medium activeTheme" : "medium"}></span>
+                <span onClick={() => setTheme("dark")} className={theme === "dark" ? "dark activeTheme" : "dark"}></span>
+                <span onClick={() => setTheme("g1")} className={theme === "g1" ? "g1 activeTheme" : "g1"}></span>
+                <span onClick={() => setTheme("g2")} className={theme === "g2" ? "g2 activeTheme" : "g2"}></span>
+                <span onClick={() => setTheme("g3")} className={theme === "g3" ? "g3 activeTheme" : "g3"}></span>
             </div>
         </header>
     )
